2cda742ec30c470fad30e3ef20524e9c3de55cbc
Rohit Waghole <rohitwaghole4@gmail.com>
2023-02-10 11:01:45 +0530

    Navigation bar added

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import Login from "./Login/Login";
 import Signup from "./SignUp/Signup";
+import Home from "./Home/Home";
 
 function App() {
   return (
     <>
-      <Login />
-      <Signup />
+      {/* <Login /> */}
+      {/* <Signup /> */}
+      <Home />
     </>
   );
 }
